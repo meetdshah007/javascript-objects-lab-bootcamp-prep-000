86746c2cd840b531252a520c46c8dfc60b3240c2
Meet Shah <meetdshah007@gmail.com>
2019-02-26 10:09:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 const recipes = {};
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  const obj = Object.assign({}, object)
+  obj[key] = value;
+  return obj;
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
+}
+
+function deleteFromObjectByKey(object, key) {
+  
+}
